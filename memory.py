13051b43a39a668b86acbfa8ade2cfd13f9fbc39
ashish <ashishjena6370@gmail.com>
2024-02-21 23:54:41 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ memory.py
@@ -0,0 +1,17 @@
+import timeit
+from memory_profiler import profile
+
+@profile
+def process_data(data):
+    processed_data = [item.upper() for item in data]
+    return processed_data
+
+@profile
+def main():
+    data = ["item " + str(i) for i in range(100)]
+
+    execution_time = timeit.timeit(lambda: process_data(data), number=1)
+    print(f"Execution time: {execution_time} seconds")
+
+if __name__ == '__main__':
+    main()
