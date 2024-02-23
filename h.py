288cea8d4320ce50ca8514e84111d76f793d36fc
ashish <ashishjena6370@gmail.com>
2024-02-23 10:58:14 +0530

    Merge remote-tracking branch 'origin/master'

--- h.py
+++ h.py
@@ -1,6 +1,6 @@
 n=int(input("enter the weight"))
 if n>2 and n%2==0:
     for i in range(2,n//2+1,2):
-        print("[",i,",",n-i,"]")
+        print("[",i,",",n-i,"]",end = " " )
 else:
     print("no")
