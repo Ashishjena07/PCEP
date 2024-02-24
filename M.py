b4e011a1c647103dc4d6e8a00b5392ad052edd64
ashish <ashishjena6370@gmail.com>
2024-02-24 10:59:40 +0530

    Merge remote-tracking branch 'origin/master'

--- M.py
+++ M.py
@@ -1,4 +1,10 @@
-num = 124.5264646
-print("%.2f"%num)
-print(round(num,3))
-print(f"{.2}f".format(num))
+# num = 124.5264646
+# print("%.2f"%num)
+# print(round(num,3))
+# print(f"{.2}f".format(num))
+
+x = int(input("enter the discount:"))
+y = int(input("enter the amount:"))
+z = y*(x/100)
+print("discount amount:",z)
+print("remain amount:",y-z)
